b9d40120e52018fdf68bceae9544e55475cf400a
Jakob Vad Nielsen <jakob@griegconnect.com>
2024-04-15 09:16:37 +0200

    Fixed distribution settings

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import copy from 'rollup-plugin-copy';
 export default {
   input: 'src/main.ts',
   output: {
-    file: 'dist/bundle.js',
+    dir: 'dist',
     format: 'esm',
   },
   plugins: [
@@ -14,7 +14,7 @@ export default {
       preferBuiltins: false, // Disable preferring built-in modules
     }),
     commonjs(),
-    typescript(),
+    typescript({declaration: true}),
     copy({
       targets: [
         { src: 'node_modules/@azure/web-pubsub-client/**/*.d.ts', dest: 'dist/types' }
